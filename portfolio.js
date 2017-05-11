96308728af58ff6f5347af834ade58e2d8f9769f
Stephanie Dover <doverstephaniem@gmail.com>
2017-05-11 00:48:57 -0700

    added three objects to my porfolio arrray, added .toHTML function

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,33 @@
 'use strict';
 
-var portfolio = {
-  
+var portfolioData = [
+{
+  title = 'Bus Mall'
+  siteURL = 'https://stephaniedover.github.io/busmall/'
+  siteImageUrl = 'img/busmall.png'
+  about: 'Bus Mall is a user research app built to help a new company decide with projects to sell.
+  The test user is given three choices and they select the item they are most likly to purchase.
+  After 25 selections, charts are shown which the marketing team can use to analyze thier products.'
 }
+{
+  title = 'Bus Mall'
+  siteURL = 'https://stephaniedover.github.io/cookie-stand/sales.html'
+  siteImageUrl = 'img/salmon-cookies.png'
+  about: 'Salmon Cookies is a famous local restaurant.
+  The Salmon Cookie site is used to help the store owner plan the number of cookies to make at each our.'
+}
+{
+  title = 'Bus Mall'
+  siteURL = 'https://stephaniedover.github.io/busmall/'
+  siteImageUrl = 'img/busmall.png'
+  about: 'Bus Mall is a user research app built to help a new company decide with projects to sell.
+  The test user is given three choices and they select the item they are most likly to purchase.
+  After 25 selections, charts are shown which the marketing team can use to analyze thier products.'
+}
+
+
+
+
+
+
+]
