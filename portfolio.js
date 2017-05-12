50b40c87a2e430cf508d7e2eecce35e8c4189433
Stephanie Dover <doverstephaniem@gmail.com>
2017-05-12 00:21:23 -0700

    working on mobile first design

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,10 @@
 'use strict';
 
-var portfolio = {
-  
+var portfolios = [];
+
+function Portfolio() {
+  this.title = portfolioData.title;
+  this.imageURL = portfolioData.siteImageURL;
+  this.siteURL = portfolioData.siteURL;
+  this.descritpion = portfolioData.about;
 }
