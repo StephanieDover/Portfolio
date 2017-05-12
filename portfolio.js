c028b001c3b118b05af7db6e532dfa40af12b5af
Stephanie Dover <doverstephaniem@gmail.com>
2017-05-12 01:41:18 -0700

    set up html for tabs handler

--- portfolio.js
+++ portfolio.js
@@ -2,29 +2,23 @@
 
 var portfolios = [];
 
-var portfolio =
-[
+var portfolioData = [
 {
-  title = 'Wine Fellows'
-  siteURL = 'https://axelmichael11.github.io/Wine_Fellows/'
-  siteImageUrl = 'img/wine-fellows.png'
-  about: 'Bus Mall is a user research app built to help a new company decide with projects to sell.
-  The test user is given three choices and they select the item they are most likly to purchase.
-  After 25 selections, charts are shown which the marketing team can use to analyze thier products.'
-}
+  title: 'Wine Fellows',
+  siteURL: 'https://axelmichael11.github.io/Wine_Fellows/',
+  siteImageUrl: 'img/wine-fellows.png',
+  about: '<p>Bus Mall is a user research app built to help a new company decide with projects to sell. The test user is given three choices and they select the item they are most likly to purchase. After 25 selections, charts are shown which the marketing team can use to analyze thier products.</p>'
+},
 {
-  title = 'Bus Mall'
-  siteURL = 'https://stephaniedover.github.io/busmall/'
-  siteImageUrl = 'img/busmall.png'
-  about: 'Bus Mall is a user research app built to help a new company decide with projects to sell.
-  The test user is given three choices and they select the item they are most likly to purchase.
-  After 25 selections, charts are shown which the marketing team can use to analyze thier products.'
-}
+  title: 'Bus Mall',
+  siteURL: 'https://stephaniedover.github.io/busmall/',
+  siteImageUrl: 'img/busmall.png',
+  about: 'Bus Mall is a user research app built to help a new company decide with projects to sell. The test user is given three choices and they select the item they are most likly to purchase. After 25 selections, charts are shown which the marketing team can use to analyze thier products.'
+},
 {
-  title = 'Salmon Cookies'
-  siteURL = 'https://stephaniedover.github.io/cookie-stand/sales.html'
-  siteImageUrl = 'img/salmon-cookies.png'
-  about: 'Salmon Cookies is a famous local restaurant.
-  The Salmon Cookie site is used to help the store owner plan the number of cookies to make at each our.'
+  title: 'Salmon Cookies',
+  siteURL: 'https://stephaniedover.github.io/cookie-stand/sales.html',
+  siteImageUrl: 'img/salmon-cookies.png',
+  about: 'Salmon Cookies is a famous local restaurant. The Salmon Cookie site is used to help the store owner plan the number of cookies to make at each our.'
 }
-]
+];
